e704d07918c7931cd383755da70eb7406f9b5f13
Steven Moyano <stevenmoyano1@gmail.com>
2021-03-15 18:22:47 -0500

    Used javascript to make changes

---
+++ main.js
@@ -0,0 +1,31 @@
+const paragraph = document.querySelector('p');
+
+paragraph.style.color = "lightblue";
+
+const heading = document.querySelector("h1");
+
+heading.style.fontSize = "48px";
+
+paragraph.innerHTML = "<p>Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.</p>";
+
+const thirteen = document.querySelector("#item-13");
+
+thirteen.style.opacity = "0.5";
+
+const third = document.querySelector("#item-3");
+
+third.innerHTML = "<li>Hi!</li>";
+
+const dinosaur = document.getElementById("dino").src="http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+
+const dinosaurImg = document.querySelector("#dino");
+
+dinosaurImg.style.height = "300px";
+
+const dogo = document.querySelector("#shiba");
+
+dogo.style.height = "300px";
+
+const sixteen = document.querySelector("#item-16");
+
+sixteen.innerHTML = "Won't get fooled again."
